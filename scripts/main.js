1beb40a0d8d52ba1fce5fd3e204418bcc2d62051
nguyenthithuns <nguyenthithunscntt@gmail.com>
2016-06-25 21:12:45 +0700

    Bai cua ban Thu_ngay 1, lam nen game

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+  var canvas= document.createElement("canvas");
+  document.body.appendChild(canvas);
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  context = canvas.getContext("2d");
+}
